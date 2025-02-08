b2f36daf8138523f4e99bda54642ab1ef1bf2c84
Santiago <mesasantiago5@gmail.com>
2025-02-08 17:47:04 -0500

    Compilacion de archivos

---
+++ Taller01_PlandePruebas/rectangle.h
@@ -0,0 +1,17 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+struct Rectangle {
+  float posX;
+  float posY;
+  int width;
+  int height;	
+};
+
+float perimeterRect ( Rectangle rect );
+float areaRect ( Rectangle rect );
+float distOriginRect ( Rectangle rect );
+
+#include "rectangle.cxx"
+
+#endif
